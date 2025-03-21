6b87f4fae4ce6ec8a42f2d158ccf98319c9d82fa
Suban05 <anatolyb94@gmail.com>
2025-03-21 17:52:38 +0500

    improves

--- lib/yaip/ipgeobase.rb
+++ lib/yaip/ipgeobase.rb
@@ -7,17 +7,25 @@ require "happymapper"
 module Yaip
   # The Ipgeobase client
   class Ipgeobase
-    def self.lookup(ip, params = {})
-      query = ""
-      query = "{?query*}" if params.any?
-      uri = Addressable::Template.new("http://ip-api.com/xml/#{ip}#{query}")
-                                 .expand("query" => params)
-      hostname = uri.hostname
-      req = Net::HTTP::Get.new(uri)
-      res = Net::HTTP.start(hostname) do |http|
-        http.request(req)
-      end
-      HappyMapper.parse(res.body)
+    def self.lookup(ip, params = {}) = new(ip, params).value
+
+    def initialize(ip, params = {})
+      @ip = ip
+      @params = params
     end
+
+    def value
+      uri = Addressable::Template.new("http://ip-api.com/xml/#{@ip}#{query}")
+                                 .expand("query" => @params)
+      HappyMapper.parse(
+        Net::HTTP.start(uri.hostname) do |http|
+          http.request(Net::HTTP::Get.new(uri))
+        end.body
+      )
+    end
+
+    private
+
+    def query = @params.any? ? "{?query*}" : ""
   end
 end
